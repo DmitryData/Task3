96ad6aef3ae6a16900fdc3fbf9b99a7c7bdde038
dmitryData <DeKar3D@hotmail.com>
2018-05-28 11:15:06 +0300

    3.	Необходимо написать программу, которая проверяет слово на "палиндромность". Слово для проверки вводится вручную во время выполнения программы.

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    boolean flagFind = true;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите слово: " + "\n");
+
+        String word = in.nextLine();
+        int len = word.length();
+        char[] wordChar = word.toCharArray();
+
+        for(int i = 0; i < len/2; ++i)
+        {
+            if(wordChar[i] != wordChar[len-i-1])
+            {
+                flagFind = false;
+            }
+        }
+
+        if (flagFind) {
+            System.out.println("Полиндром ");
+        } else {
+            System.out.println("Не полиндром ");
+        }
+
+    }
+}
